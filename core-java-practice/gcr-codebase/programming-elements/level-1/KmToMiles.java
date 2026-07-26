c5818a424d726ee44cb929007b00ac3d70b4b36c
SrishtiSachdeva17 <17.srish@gmail.com>
2026-07-26 13:30:09 +0530

    Modified files

--- core-java-practice/gcr-codebase/programming-elements/level-1/KmToMiles.java
+++
@@ -1,7 +0,0 @@
-public class KmToMiles {
-    public static void main(String[] args) {
-        int km = 10;
-        int miles = (int) (km / 1.6);
-        System.out.println("The distance " + km + " km in miles is " + miles);
-    }
-}
